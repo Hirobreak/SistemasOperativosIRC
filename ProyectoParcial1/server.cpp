6f905470080e4457cd316584dcd12699d34fd0b5
Pedro Aim <pedro_aim@hotmail.com>
2015-12-14 02:43:31 -0500

    Productor-Consumidor y Chat global

--- ProyectoParcial1/server.cpp
+++ ProyectoParcial1/server.cpp
@@ -10,6 +10,8 @@
 #include<algorithm>
 #include<time.h>
 #include<cassert>
+#include<pthread.h>
+#include<sys/fcntl.h>
 
 using namespace std;
 
@@ -19,8 +21,12 @@ struct usuario {
   int channel;
 } ;
 
+pthread_mutex_t buff, full, empty;
+int sender = -1;
 int bufsize = 512;
 char buffer[512];
+int msize = 1024;
+char mensaje_server[1024];
 bool isExit = false;
 string names[10];
 usuario usuarios[25];
@@ -28,6 +34,7 @@ time_t start = time(0);
 char builtTime[32];
 char startTime[32];
 void *connection_handler(void *);
+void *server_handler(void *socket_desc);
 void parse_command(char *textInput, int socket);
 void show_info(int socket);
 void builtDateTime();
@@ -76,9 +83,11 @@ int main(){
 	cout << "Listening for clients" << endl;
 	listen(server, 3);
 	pthread_t thread_id;
+	pthread_create( &thread_id , NULL ,  server_handler, (void*) &server);
 
 	while(client = accept(server, (struct sockaddr*)&client_addr, &size)){
 		cout << "Connected with client..." << endl;
+		fcntl(client, F_SETFL, O_NONBLOCK);
 		if( pthread_create( &thread_id , NULL ,  connection_handler , (void*) &client) < 0)
         {
             cout << "Couldn't create thread" << endl;
@@ -93,62 +102,6 @@ int main(){
 		exit(1);
 	}
 
-	/*while (server > 0){
-		memset(buffer, 0, bufsize);
-		strcpy(buffer, "Hello, Hello! Server connected...\n");
-		send(server, buffer, bufsize, 0);
-		memset(buffer, 0, bufsize);
-
-		cout << "Connected with client..." << endl;
-		cout << "Enter # to end connection" << endl;
-
-		cout << "Client: ";
-		do {
-			recv(server, buffer, bufsize, 0);
-			cout << buffer << "";
-			if (*buffer == '#'){
-				*buffer = '*';
-				isExit = true;
-			}else{
-				memset(buffer, 0, bufsize);
-			}
-		} while (*buffer != '*');
-		memset(buffer, 0, bufsize);
-		do {
-			cout << "\nServer: ";
-			do {
-				cin >> buffer;
-				strcat(buffer, "\n");
-				send(server, buffer, bufsize, 0);
-				if (*buffer == '#'){
-					send(server, buffer, bufsize, 0);
-					*buffer = '*';
-					isExit = true;
-				}else{
-					memset(buffer, 0, bufsize);
-				}
-			} while(*buffer != '*');
-			memset(buffer, 0, bufsize);
-			cout << "Client: ";
-
-			do{
-				recv(server, buffer, bufsize, 0);
-				cout << buffer << "";
-				if (*buffer == '#'){
-					*buffer = '*';
-					isExit = true;
-				}else{
-					memset(buffer, 0, bufsize);
-				}
-			} while (*buffer != '*');
-
-		} while (!isExit);
-
-		cout << "Connection terminated..." << endl;
-		cout << "Goodbye..." << endl;
-		isExit = false;
-		exit(1);
-	}*/
 	close(server);
 	return 0;
 }
@@ -157,34 +110,48 @@ void *connection_handler(void *socket_desc)
 {
     //Get the socket descriptor
     int sock = *(int*)socket_desc;
-    
+    int length = 0;
     //char mensaje[512];
     string nombre;
 
-    memset(buffer, 0, bufsize);
+    //memset(buffer, 0, bufsize);
 	strcpy(buffer, "Connected...\n");
 	strcpy(buffer, "For info about this server type /INFO\n");
 	send(sock, buffer, bufsize, 0);
-	memset(buffer, 0, bufsize);
+	//memset(buffer, 0, bufsize);
 
 	do {
-		recv(sock, buffer, bufsize, 0);
+		pthread_mutex_lock(&empty);//SECCION CRITICA
+		pthread_mutex_lock(&buff);
+		memset(buffer, 0, bufsize);
+		length = recv(sock, buffer, bufsize, 0);
+		if(length < 1){
+			sender = -1;
+		}else{
+			sender = sock;
+			cout << "mensaje de " << sock << endl;
+		}
 
-		//strcpy(mensaje, buffer);
-		if (usuarios[sock].nombre.empty()){
-			cout << "Anonimo " << sock << ": ";
-		}else{
-			cout << usuarios[sock].nombre << ": ";
+		if(length >= 1){
+			cout << "Im here " << sock << endl;
+			//strcpy(mensaje, buffer);
+			if (usuarios[sock].nombre.empty()){
+				cout << "Anonimo " << sock << ": ";
+			}else{
+				cout << usuarios[sock].nombre << ": ";
+			}
+			cout << buffer << "";
+			if (*buffer == '#'){
+				*buffer = '*';
+				isExit = true;
+			}else if (*buffer == '/'){
+				parse_command(buffer, sock);
+			}else{
+				//memset(buffer, 0, bufsize);
+			}
 		}
-		cout << buffer << "";
-		if (*buffer == '#'){
-			*buffer = '*';
-			isExit = true;
-		}else if (*buffer == '/'){
-			parse_command(buffer, sock);
-		}else{
-			memset(buffer, 0, bufsize);
-		}
+		pthread_mutex_unlock(&buff);
+		pthread_mutex_unlock(&full);
 		
 	} while (*buffer != '*');
     
@@ -261,6 +228,7 @@ void change_nickname(int socket){
 	char* mensaje = (char*)calloc(strlen(buffer)+1, sizeof(char));
 	char * word;
 	strcpy(mensaje, buffer);
+	cout << "que hay en el buffer " << buffer << endl;
 	word = strtok(mensaje, " \r");
 	word = strtok(NULL, " \r");
 	if (sizeof(word)>0){
@@ -271,3 +239,36 @@ void change_nickname(int socket){
 	}
 	free(mensaje);
 }
+
+void *server_handler(void *server_desc){
+
+ 	while(true){
+		pthread_mutex_lock(&full);
+		pthread_mutex_lock(&buff);
+
+		if(sender > 0){
+			cout << "por aqui ando" << endl;
+			for( int i = 0; i < 25; i++){
+				memset(mensaje_server, 0, msize);
+				if(usuarios[sender].nombre.empty()){
+					strcat(mensaje_server, "Anonimo");
+				}else{
+					strcat(mensaje_server, usuarios[sender].nombre.c_str());
+				}
+				strcat(mensaje_server, ": ");
+				strcat(mensaje_server, buffer);
+
+				if(i != sender && !usuarios[i].nombre.empty()){
+					//strcpy(mensaje_server, "Habla Flaco!!! ");
+					send(i, mensaje_server, msize, 0);
+				}
+			}
+			cout << "liberar locks" << endl;
+			sender = -1;
+		}
+		
+		pthread_mutex_unlock(&buff);
+		pthread_mutex_unlock(&empty);
+	}
+    return 0;
+} 
